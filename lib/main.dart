b88c3d3ac8576673ea1d52694e5be554aaee3b2c
Gamalliel Sharon <you@example.com>
2020-09-02 19:06:34 +0700

    My first Flutter mobile apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,83 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.transparent,
+              backgroundImage: AssetImage('images/thor.jpg'),
+            ),
+            Text(
+              'Thor',
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+            Text(
+              'MARVEL UNIVERSE',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                letterSpacing: 2.5,
+                fontFamily: 'Source Sans Pro',
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 200.0,
+              child: Divider(
+                color: Colors.teal[100],
+              ),
+            ),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+6288852999',
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.teal),
+                  ),
+                )),
+            Card(
+              margin: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
+              color: Colors.white,
+              child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'hello.thor@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                      color: Colors.teal,
+                    ),
+                  )),
+            )
+          ]),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
